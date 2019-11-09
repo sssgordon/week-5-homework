7c15a02c558b946800a52ff96876d8e801d62ba3
sssgordon <gso7@uwo.ca>
2019-11-09 11:38:11 +0100

    Added middleware to limit requests to 5

--- messages-api.js
+++ messages-api.js
@@ -8,11 +8,23 @@ const textPropValidatorMiddleware = (req, res, next) => {
   } else next();
 };
 
+let requests = 0;
+const requestCounterMiddleware = (req, res, next) => {
+  if (requests >= 5) {
+    res.status(429).end();
+  } else {
+    next();
+  }
+};
+
 const jsonParser = bodyParser.json();
 app.use(jsonParser);
 
+app.use(requestCounterMiddleware);
+
 app.post("/messages", textPropValidatorMiddleware, (req, res, next) => {
   console.log(req.body.text);
+  requests++;
   res.send({ message: "Message received loud and clear" });
 });
 
